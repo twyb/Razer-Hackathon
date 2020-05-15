885b0befe24697235003d362647110a84ec5d7bb
Darren Tan Yong Heng <darren.tanyongheng@gmail.com>
2020-05-15 20:49:40 +0800

    included jsonify

--- Backend/App.py
+++ Backend/App.py
@@ -3,24 +3,24 @@ import socket
 from flask_cors import CORS, cross_origin
 from backend import app
 from cheroot import wsgi
-# from backend.routes.services import services
 from flask import Blueprint
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify
 
+# from backend.routes.services import services
 # from Backend.routes.login import twofa
 
 app = Flask(__name__)
 cors = CORS(app)
 # app.register_blueprint(services)
 
-@app.route('/loans')
+@app.route('/loans', methods = ['GET', 'POST '])
 def loans():
-    return "Hello World!"
+    # return "Hello World! Hey!"
+    return jsonify({"about": "Hello World!"})
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
-formatter = logging.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
