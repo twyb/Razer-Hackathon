6756618e7072543afad213b35eb8c9e79629047e
Darren Tan Yong Heng <darren.tanyongheng@gmail.com>
2020-05-16 21:49:10 +0800

    Calling the FWD eKYC API and included NRIC images

---
+++ FWD_eKYC.py
@@ -0,0 +1,33 @@
+import json
+import requests
+import pprint
+import base64
+import urllib.request
+
+
+with open("images/NRIC_Front.png", "rb") as img_file_1:
+    my_string = base64.b64encode(img_file_1.read())
+
+image_string = my_string.decode('utf-8')
+# print(my_string)
+
+# name = razer-group-039
+# token_key = YM1BqxFUcjnY9G6ADuRt
+  
+# defining the api-endpoint  
+API_ENDPOINT = "https://niw1itg937.execute-api.ap-southeast-1.amazonaws.com/Prod/verify"
+  
+# your API key here 
+API_KEY = "YM1BqxFUcjnY9G6ADuRt"
+
+headers = {'content-type': 'application/json'}
+  
+# data to be sent to api 
+payload = {'base64image':image_string} 
+  
+# sending post request and saving response as response object 
+r = requests.post(url = API_ENDPOINT, data = json.dumps(payload), headers = headers) 
+  
+# extracting response text  
+result = r.text 
+print(result) 
